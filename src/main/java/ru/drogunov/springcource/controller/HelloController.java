22956cc073939d3eddb2dfcb41628847ff1a336f
sergey <drogunov-s@yandex.ru>
2023-02-11 13:21:03 +0300

    5, 6. Параметры GET запроса. Аннотация @RequestParam
    HTTP,Request,Responce,Вводный
    @RequestParam,Request

--- src/main/java/ru/drogunov/springcource/controller/HelloController.java
+++ src/main/java/ru/drogunov/springcource/controller/HelloController.java
@@ -2,12 +2,22 @@ package ru.drogunov.springcource.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/first")
 public class HelloController {
-    @GetMapping("/hello-world")
-    public String helloSay() {
-        return "hello_world";
+    @GetMapping("/hello")
+    public String helloSay(@RequestParam(name = "name", required = false) String name,
+                           @RequestParam(name = "surname", required = false) String surname) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+        System.out.println(name + " " + surname);
+        return "/first/hello";
     }
     
 }

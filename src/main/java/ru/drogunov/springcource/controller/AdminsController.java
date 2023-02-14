d7d659da8c8b3ba046fda238be2a576575753ae2
sergey <drogunov-s@yandex.ru>
2023-02-14 10:00:31 +0300

    Выпадающие списки
    Некрасивый хак в классе PersonDAO строка: 86
    Пояснение там же.
    
    <option>,<select>,JdbcTemplate

--- src/main/java/ru/drogunov/springcource/controller/AdminsController.java
+++ src/main/java/ru/drogunov/springcource/controller/AdminsController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.drogunov.springcource.dao.PersonDAO;
 import ru.drogunov.springcource.model.Person;
 
-import java.util.Map;
-
 @Controller
 @RequestMapping("/admin")
 public class AdminsController {
@@ -21,18 +19,14 @@ public class AdminsController {
     
     @GetMapping
     public String index(Model model,
-                        @ModelAttribute("person") Person person
-    ) {
+                        @ModelAttribute("person") Person person) {
         model.addAttribute("people", personDAO.index());
-//        model.addAttribute("role", person.getRole());
         return "admin/admin";
     }
     
     @PatchMapping("/add")
-    public String add(@ModelAttribute("person") Person person, Model model,
+    public String add(@ModelAttribute("person") Person person,
                       @RequestParam("role") String role) {
-        Map<String, Object> stringObjectMap = model.asMap();
-//        String role = (String) model.getAttribute("role");
         personDAO.updateRole(person, role);
         return "redirect: /admin";
     }
